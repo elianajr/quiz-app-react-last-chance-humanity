ec7af08b6cb6e5c939e6ee23d5b58cb85dfea830
Eliana Jordan <76211124+elianajr@users.noreply.github.com>
2022-10-24 11:37:28 +0200

    funcionalities

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -5,10 +5,13 @@ import '../../../src/styles/home.css';
 export default function Home() {
 
     return(
-        <div>
+        <div >
             <h2>
-                Welcome to the last chance for humanity quizz. It's time to show your human's value against machines
+                Welcome to the last chance for humanity quizz
             </h2>
+            <h4>
+                It's time to show your human's value against machines
+            </h4>
             <button onClick={() => (window.location.href = "/quiz")}>
                 Start quizz now
             </button>
--- src/js/pages/Quiz.js
+++ src/js/pages/Quiz.js
@@ -2,72 +2,75 @@ import React, { useState } from 'react';
 import questions from "../components/Questions"
 
 export default function App() {
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [showAnswer, setShowAnswer] = useState(false);
+	const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
+    const [score, setScore] = useState(0);
+
+    const handleAnswerSubmit = (isCorrect, index) => {
+		setSelectedOptionIndex(index);
+        if (isCorrect) {
+            setScore(score + 1);
+        }
+
+        setShowAnswer(true);
+
+        const nextQuestion = currentQuestion + 1;
+        setShowScore(nextQuestion > questions.length);
+    };
+
+    const nextQuestionButton = () => {
+		setSelectedOptionIndex(null);
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setShowAnswer(false);
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
 
 
-	const handleAnswerSubmit = (isCorrect, e) => {
-		if (isCorrect) {
-			setScore(score + 1);
-			alert('correct');
-		}
+    return (
+        <div className='app'>
+            {showScore ? (
+                <div className='score-section'>
+                    <span>
+                        You scored {score} out of {questions.length}
+                    </span>
+                    <button onClick={() => (window.location.href = "/")}>
+                        Play again
+                    </button>
+                </div>
+            ) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Question {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answer, index) => (
+                            <button
+								type="button"
+                                key={answer.answerOptions}
+                                className={showAnswer && (selectedOptionIndex === index || answer.isCorrect) ? (answer.isCorrect ? 'correct' : 'incorrect') : ''}
+                                disabled={showAnswer}
+                                onClick={() => handleAnswerSubmit(answer.isCorrect, index)}>
+                                    {answer.answerText}
+                            </button>
+                        ))}
 
-		if (isCorrect === true) {
-			e.target.classList.add('correct')
-			e.target.classList.remove('incorrect')
-		} else {
-			e.target.classList.add('incorrect')
-			e.target.classList.remove('correct')
-		}
+                        <button onClick={() => (window.location.href = "/quit")}>
+                            Quit
+                        </button>
 
-
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-
-	};
-
-
-	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>
-					<span>
-						You scored {score} out of {questions.length}
-					</span>
-					<button onClick={() => (window.location.href = "/")}>
-						Play again
-					</button>
-				</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{questions.length}
-						</div>
-						<div className='question-text'>{questions[currentQuestion].questionText}</div>
-					</div>
-					<div className='answer-section'>
-						{questions[currentQuestion].answerOptions.map((answer) => (
-							<button
-								/* className={colorClass ? "correct" : "incorrect"} */
-								key={answer.answerOptions}
-								onClick={(e) => handleAnswerSubmit(answer.isCorrect, e)}>
-									{answer.answerText}
-							</button>
-						))}
-
-						<button onClick={() => (window.location.href = "/quit")}>
-							Quit
-						</button>
-						{/* <button onClick={(e) => nextQuestionButton()}>Next Question</button> */}
-					</div>
-				</>
-			)}
-		</div>
-	);
+                        <button onClick={() => nextQuestionButton()} disabled={!showAnswer}>Next Question</button>
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
